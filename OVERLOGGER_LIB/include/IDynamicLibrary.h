bdbe9dffa61b7777ecf93f60b3392a4ae9d551e0
ivo <ivo.alani@globant.com>
2022-04-22 17:12:48 -0300

    commit de Fran

--- OVERLOGGER_LIB/include/IDynamicLibrary.h
+++ OVERLOGGER_LIB/include/IDynamicLibrary.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <memory>
+#include <string>
 
 namespace olg::dl {
+    class IDynamicLibraryFunctionPointer;
 	class IDynamicLibrary {
-
+    public:
+        virtual std::unique_ptr<IDynamicLibraryFunctionPointer> getFunction(const std::string &functionName) const = 0;
 	};
 }
