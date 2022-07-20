1a266e98c17e98b17f70ebbaf37cc6dcdcd38c1a
Didier Renard <didier.renard@globant.com>
2022-07-20 15:48:00 -0300

    Empezando implementacion POSIX

--- OVERLOGGER_DLL/include/DllUtils.h
+++ OVERLOGGER_DLL/include/DllUtils.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
+	#define DLLEXPORT __declspec(dllexport)
+#elif defined(__GNUC__)
+	#define DLLEXPORT __attribute__((visibility("default")))
 #else
-#error "TODO other platforms"
+	#error "TODO other platforms"
 #endif
---
+++ OVERLOGGER_DLL/src/CallStackRetriever_Linux.cpp
@@ -0,0 +1,80 @@
+#include <execinfo.h>
+#include <dlfcn.h>
+#include <cxxabi.h>
+
+#include "ICallStack.h"
+#include "CallStackRetriever.h"
+#include "CallStackFactory.h"
+#include "CallStackFrame.h"
+#include "CallStackFrameNull.h"
+#include <iostream>
+
+constexpr unsigned int TRACE_MAX_STACK_FRAMES = 64;
+
+namespace olg
+{
+    class CallStackRetrieverImpl {
+    public:
+        std::unique_ptr<ICallStackFactory> mCallStackFactory;
+        CallStackRetrieverImpl(std::unique_ptr<ICallStackFactory>&& callStackFactory)
+            : mCallStackFactory{ std::move(callStackFactory) }
+        {}
+    };
+
+    CallStackRetriever::CallStackRetriever(std::unique_ptr<ICallStackFactory>&& callStackFactory)
+        : mImpl{ std::make_unique<CallStackRetrieverImpl>(std::move(callStackFactory)) }
+    {}
+
+    CallStackRetriever::~CallStackRetriever()
+    {
+    }
+
+    std::unique_ptr<ICallStack> CallStackRetriever::retrieve()
+    {
+        void* callstack[TRACE_MAX_STACK_FRAMES] { 0 };
+        const int numberOfFrames = backtrace(callstack, TRACE_MAX_STACK_FRAMES);
+        char** symbols = backtrace_symbols(callstack, numberOfFrames);
+
+        if (symbols == nullptr)
+        {
+            std::cerr << "Error when retrieving backstrace_symbols\n";
+            return nullptr;
+        }
+
+        std::vector<std::unique_ptr<ICallStackFrame>> frames;
+
+        for (int i = 0; i < numberOfFrames; i++)
+        {
+            std::unique_ptr<ICallStackFrame> frame;
+
+            // TODO: ver de donde sacar filename y linenumber
+
+            Dl_info info; //TODO: ver que trae este DL_info. https://man7.org/linux/man-pages/man3/dladdr.3.html
+            if (dladdr(callstack[i], &info) != 0)
+            {
+                int status = 0;
+                char* demangled = abi::__cxa_demangle(info.dli_sname, nullptr, 0, &status);
+                std::string functionName = std::string((status == 0)? demangled) : std::string(info.dli_sname);
+                frame = std::make_unique<CallStackFrame>(reinterpret_cast<unsigned long long>(callstack[i]), std::string(demangled), /*line.FileName*/ "", /*line.LineNumber*/ 0);
+                if(demangled != nullptr)
+                {
+                    free(demangled);
+                }
+            }
+            else
+            {
+                frame = std::make_unique<CallStackFrame>(reinterpret_cast<unsigned long long>(callstack[i]), "NO NAME INFO", /*line.FileName*/ "", /*line.LineNumber*/ 0);
+            }
+            frames.push_back(std::move(frame));
+        }
+
+        free(symbols);
+        return mImpl->mCallStackFactory->create(std::move(frames));
+    }
+
+}
+
+olg::ICallStackRetriever* createCallStackRetriever()
+{
+    return new olg::CallStackRetriever{ std::make_unique<olg::CallStackFactory>() };
+}
